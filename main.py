088eea594e2802a11bb90e2bc91e1109097b1f79
xylarshayu <xylar.w@gmail.com>
2023-05-13 20:55:29 +0530

    added methods

--- main.py
+++ main.py
@@ -8,3 +8,24 @@ class NeuralNetwork:
     self.bias_h = np.random.normal(0.0, 0.1, (2, 1))
     self.bias_o = np.random.normal(0.0, 0.1, (2, 1))
 
+  def tanh(self, x):
+    return np.tanh(x)
+
+  def derivative_tanh(self, x):
+    return 1 - np.tanh(x) ** 2
+
+  def forward(self, input):
+    self.hidden = self.tanh(np.dot(self.weights_ih, input) + self.bias_h)
+    self.output = self.tanh(np.dot(self.weights_ho, self.hidden) + self.bias_o)
+    return self.output
+
+  def backward(self, input, target):
+    output_errors = target - self.output
+    hidden_errors = np.dot(self.weights_ho.T, output_errors)
+
+    # Delta rule for updating weights and biases
+    self.weights_ho += self.learning_rate * np.dot((output_errors * self.derivative_tanh(self.output)), self.hidden.T)
+    self.bias_o += self.learning_rate * (output_errors * self.derivative_tanh(self.output))
+    self.weights_ih += self.learning_rate * np.dot((hidden_errors * self.derivative_tanh(self.hidden)), input.T)
+    self.bias_h += self.learning_rate * (hidden_errors * self.derivative_tanh(self.hidden))
+
